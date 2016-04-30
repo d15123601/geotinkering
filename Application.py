9d68b82516bb31894b6d40f5ddf40b2542ff1e80
mickleod <mickleod@gmail.com>
2016-04-30 15:15:10 +0100

    Gui shaping up, working on shapes class

--- Application.py
+++ Application.py
@@ -1,6 +1,5 @@
 """ Here we access geojson held in a textfile - it is then used to perform the
-    following tasks reqd for the GIS programming assignment..... see the 'ni' file
-    to use code which accesses the geoserver.
+    following tasks reqd for the GIS programming assignment.....
 
     Specifically, we want to do the following:
 
@@ -19,24 +18,173 @@
     Linestring representing the distance between the centroids
 
 """
-def main():
 
-    import cartopy
-    import matplotlib.pyplot as plt
-    from geo_utils import get_data_from_geoserver, geocode_item
-    from geo_utils import shape_maker
-    from fiona import collection
-    from fiona.crs import from_epsg
-    import fiona
-    from shapely import geometry
-    import pyproj
-    import geopy
-    from shapely.geometry import asShape
-    from shapely.ops import cascaded_union
-    import collections
-    import os
-    import json
-    from descartes import PolygonPatch
+from tkinter import *
+from tkinter import ttk
+import cartopy
+import matplotlib.pyplot as plt
+from geo_utils import get_data_from_geoserver, geocode_item
+from geo_utils import shape_maker
+from fiona import collection
+from fiona.crs import from_epsg
+import fiona
+from shapely import geometry
+import pyproj
+import geopy
+from shapely.geometry import asShape
+from shapely.ops import cascaded_union
+import collections
+import os
+import json
+from descartes import PolygonPatch
+
+class shapes(geojson_obj):
+    def __init__(self):
+
+
+
+class myGUI:
+    def __init__(self, master, input_datasets):
+        # variables
+        self.dataset_ref = ""
+        self.ops_output = {}
+
+        self.dialog_text = StringVar()
+        self.dialog_text.set("Messages will display here")
+
+        self.dataset_list = [keys for keys in input_datasets.keys()]
+        self.dataset = input_datasets
+        self.cb_list = ['No data loaded']
+
+        self.ops_subject = StringVar()
+        self.ops_subject.set('')
+
+        # GUI creation
+        self.master = master
+
+        # Initialise all the widgets.
+        self.main_frame = ttk.Frame(self.master)
+
+        self.dialog = ttk.Label(self.main_frame, textvariable = self.dialog_text,
+                                foreground = 'cyan',
+                                background = 'blue', anchor = 'center')
+        self.selection_box = ttk.Labelframe(self.main_frame, text = 'Object Picker',
+                                width = 250, height = 500, borderwidth = 5,
+                                relief = 'sunken')
+        self.dataset_box = ttk.Labelframe(self.selection_box, text = 'Dataset Picker',
+                                width = 250, height = 100, borderwidth = 5,
+                                relief = 'sunken')
+        ttk.Label(self.dataset_box, text = 'Please choose a dataset from the list:',
+                  foreground = 'red', justify = 'left').grid(row = 0, column = 0)
+        self.datacb = ttk.Combobox(self.dataset_box, values= self.dataset_list, state='readonly')
+
+        self.datacb.current(1)
+        # set the selection event
+        self.cb_box = ttk.Labelframe(self.selection_box, text = 'Item Picker',
+                                width = 250, height = 400, borderwidth = 5,
+                                relief = 'sunken')
+        ttk.Label(self.cb_box, text = 'Please choose an item from the list:',
+                  foreground = 'red', justify = 'left').grid(row = 0, column = 0)
+        self.itemcb = ttk.Combobox(self.cb_box, values= self.cb_list, state='disabled')
+        self.itemcb.current(0)
+        # set the selection event
+        self.op_box = ttk.Labelframe(self.main_frame, text = 'Operations',
+                                    width = 250, height = 500,
+                                    borderwidth = 5,
+                                    relief = 'sunken')
+        self.btn_add = ttk.Button(self.op_box, width = 50, text = 'Add', command = self.choose_selected)
+        self.btn_merge = ttk.Button(self.op_box, width = 50, text = 'Merge',
+                                    command = self.merge_chosen, state = 'disabled')
+        self.btn_geocode = ttk.Button(self.op_box, width = 50, text = 'Geocode',
+                                    command = self.geocode_chosen, state = 'disabled')
+        self.btn_centroid = ttk.Button(self.op_box, width = 50, text = 'Centroid',
+                                    command = self.centroid_chosen, state = 'disabled')
+        self.btn_convexhull = ttk.Button(self.op_box, width = 50, text = 'Make Convex Hull',
+                                    command = self.convex_hull, state = 'disabled')
+        self.btn_line_join = ttk.Button(self.op_box, width = 50, text = 'Join with line',
+                                    command = self.line_join, state = 'disabled')
+        self.ops_frame = ttk.Labelframe(self.op_box, text = 'Items selected:',
+                                        relief = 'sunken')
+        self.subject_list = ttk.Label(self.ops_frame, textvariable = self.ops_subject,
+                                      anchor = 'w')
+        self.display_box = ttk.Labelframe(self.main_frame, text = 'Object Display',
+                                width = 500, height = 500, borderwidth = 5,
+                                relief = 'sunken')
+
+
+        #Lay out all the widgets
+        self.main_frame.grid(column = 0, row = 0, sticky = (N, S, E, W))
+        self.dialog.grid(row = 0, column = 0, columnspan = 3)
+        self.selection_box.grid(row = 1, column = 0, sticky = 'ns')
+        self.dataset_box.grid(row = 0, column = 0, sticky = 'ns')
+        self.datacb.grid(row = 1, column = 0, sticky = 'nsew')
+        self.cb_box.grid(sticky = 'nsew')
+        self.itemcb.grid(row = 1, column = 0, sticky = 'nsew')
+        self.op_box.grid(row = 1, column = 1, sticky = 'ns')
+        self.btn_add.grid(row = 0, column = 0)
+        self.btn_merge.grid(row = 1, column = 0)
+        self.btn_geocode.grid(row = 2, column = 0)
+        self.btn_centroid.grid(row = 3, column = 0)
+        self.btn_convexhull.grid(row = 4, column = 0)
+        self.btn_line_join.grid(row = 5, column = 0)
+        self.ops_frame.grid(sticky = 'nsew')
+        self.subject_list.grid(sticky = 'nsew')
+        self.display_box.grid(row = 1, column = 2, sticky = 'ns')
+
+        # Event handling
+        self.datacb_value = self.datacb.bind("<<ComboboxSelected>>", self.dataset_cb_newselection)
+        self.itemcb_value = self.itemcb.bind("<<ComboboxSelected>>", self.itemcb_newselection)
+
+    def dataset_cb_newselection(self, event):
+        print(event.widget)
+        owner = event.widget
+        self.value_of_combo = owner.get()
+        self.dialog_text.set("You have chosen - " + self.value_of_combo)
+        self.update_itemcb(self.value_of_combo)
+
+    def itemcb_newselection(self, event):
+        print(event.widget)
+        owner = event.widget
+        self.geo_item = owner.get()
+
+
+
+    def update_itemcb(self, dataset_name):
+        item_list = [i[0] for i in self.dataset[dataset_name]]
+        self.cb_list = 'Please select from below'
+        self.itemcb['values'] = item_list
+        self.itemcb.state(['!disabled', 'readonly'])
+
+
+    def add_cb_value(self):
+        self.cty = str(self.dataset_cb_newselection)
+        self.cty_stack.append(self.cty)
+
+    def choose_selected(self):
+        pass
+
+
+    def merge_chosen(self):
+        #TODO ensure >1 argument to this function
+        pass
+
+
+    def geocode_chosen(self):
+        #TODO ensure only 1 argument to this function
+        pass
+
+    def centroid_chosen(self):
+        pass
+
+
+    def convex_hull(self):
+        pass
+
+
+    def line_join(self):
+        pass
+
+def main():
 
     #   Variable assignment or initialisation
     countycents = collections.defaultdict(str)
@@ -56,18 +204,18 @@ def main():
     counties = shape_maker(cty_polygons)
     towns = shape_maker(places_pts)
 
+    root = Tk()
+    county_names = [(c[1]['countyname'],c[0]) for c in counties['features']]
+    town_names = [(t[1]['name'],t[0]) for t in towns['features']]
+    datasets = {'Counties':county_names, 'Towns':town_names}
+    myGUI(root, datasets)
+    root.mainloop()
 
     print(len(counties['features']))
     print(counties['crs'])
     print(len(towns['features']))
     print(towns['crs'])
 
-    #map_crs = cartopy.crs.TransverseMercator()
-    #data_crs = cartopy.crs.TransverseMercator()
-    ax = plt.axes([counties['bbox']])
-    patch1 = PolygonPatch(geoms[1][0])
-    ax.add_patch(patch1)
-    plt.show()
 
 
 
--- gui_dummy.py
+++ gui_dummy.py
@@ -1,5 +1,10 @@
-from geo_utils import shape_maker
+from geo_utils import shape_maker, plot_shapes
 import json
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+from mpl_toolkits.basemap import Basemap
+from shapely.ops import cascaded_union
+from descartes import PolygonPatch
 
 with open("cso_counties.txt",'r') as f1:
         cty_str = f1.read()
@@ -13,6 +18,24 @@ places_pts = json.loads(pop_str)
 counties = shape_maker(cty_polygons)
 towns = shape_maker(places_pts)
 
+cty_features = []
+for cf in counties['features']:
+    cty_features.append(cf)
+
+patches = []
+for cf in cty_features:
+    if cf.geom_type == 'Polygon':
+        patches.append(PolygonPatch(cf))
+    elif cf.geom_type == 'MultiPolygon':
+        for poly in cf:
+            patches.append(PolygonPatch(poly))
 
 
-plot_shapes(counties)
+for patch in patches:
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    m = Basemap(llcrnrlon=-11.5,llcrnrlat=51.0,urcrnrlon=-5.0,urcrnrlat=56.0,
+            resolution='i',projection='tmerc',lon_0=-7.36,lat_0=53.0, epsg = 29902)
+    ax.add_patch(patch)
+    plt.show()
+
